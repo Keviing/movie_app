2a1a6437847a94fad020073c2d8a0abcfabba4f6
Keviing <gualpakevin@gmail.com>
2026-02-16 15:42:21 -0500

    se coloco los actores en pantalla de pelicula

--- lib/presentation/movie/movie_only_screen.dart
+++ lib/presentation/movie/movie_only_screen.dart
@@ -1,8 +1,9 @@
+import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/domain/entities/movie.dart';
+import 'package:movie_app/presentation/provider/actor/actor_movie_respository.dart';
 import 'package:movie_app/presentation/provider/movie/movie_info_provider.dart';
-import 'package:movie_app/presentation/widgets/widgets.dart';
 
 class MovieOnlyScreen extends ConsumerStatefulWidget {
   //Va recibir un parametro con el id de la pelicula
@@ -19,6 +20,7 @@ class MovieOnlyScreenState extends ConsumerState<MovieOnlyScreen> {
   void initState() {
     super.initState();
     ref.read(movieInfoProvider.notifier).loadMovie(widget.movieId);
+    ref.read(listActor.notifier).getActorsMovie(widget.movieId);
   }
 
   @override
@@ -30,20 +32,21 @@ class MovieOnlyScreenState extends ConsumerState<MovieOnlyScreen> {
   Widget build(BuildContext context) {
     final Movie? movie = ref.watch(movieInfoProvider)[widget.movieId];
     if (movie == null) {
-      return Scaffold(body: Center(child: CircularProgressIndicator()));
+      return Scaffold(body: Center(child: CircularProgressIndicator(strokeWidth: 2,)));
     }
     return Scaffold(
       body: CustomScrollView(
         slivers: [
           _CustomSliverAppbar(movie: movie),
-          
-          SliverList(delegate: SliverChildBuilderDelegate(
-            (context, index) => _MovieDeatils(movie: movie),
-            childCount: 1
-          ))
-          ]
 
-        ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) => _MovieDeatils(movie: movie),
+              childCount: 1,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
@@ -66,51 +69,103 @@ class _MovieDeatils extends StatelessWidget {
             children: [
               ClipRRect(
                 borderRadius: BorderRadius.circular(20),
-                child: Image.network(
-                  movie.posterPath,
-                  width: size.width * 0.3,
-                  )
+                child: Image.network(movie.posterPath, width: size.width * 0.3),
+              ),
+
+              const SizedBox(width: 10),
+
+              SizedBox(
+                width: (size.width - 40) * 0.7,
+                child: Column(
+                  children: [
+                    Text(movie.title, style: style.titleLarge),
+                    Text(movie.overview),
+                  ],
                 ),
-
-                const SizedBox(width: 10,),
-
-                SizedBox(
-                  width: (size.width - 40) * 0.7 ,
-                  child: Column(
-                    children: [
-                      Text(movie.title, style: style.titleLarge,),
-                      Text(movie.overview)
-                    ],
-
-                  ),
-                  )
-          ],),
+              ),
+            ],
+          ),
         ),
 
-        //Genero de la pelicula 
-
+        //Genero de la pelicula
         Padding(
           padding: EdgeInsets.all(8),
           child: Wrap(
             children: [
-              ...movie.genreIds.map((gender) => Container(
-                margin: EdgeInsets.only(right: 10),
-                child: Chip(
-                  label: Text(gender),
-                  shape:  RoundedRectangleBorder( borderRadius:  BorderRadius.circular(20)),
+              ...movie.genreIds.map(
+                (gender) => Container(
+                  margin: EdgeInsets.only(right: 10),
+                  child: Chip(
+                    label: Text(gender),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20),
+                    ),
                   ),
-              ))
+                ),
+              ),
             ],
+          ),
+        ),
+        SizedBox(height: 10),
 
-          ),
-          
-          ),
-        SizedBox(height: 100,)
+        _CustomActor(movieId: movie.id,),
       ],
     );
   }
 }
 
+class _CustomActor extends ConsumerWidget {
+  final int movieId;
+  const _CustomActor({required this.movieId});
+
+  @override
+  Widget build(BuildContext context, ref) {
+
+    final actorByMovie = ref.watch(listActor);
+    if(actorByMovie[movieId.toString()] == null)
+    {
+      return CircularProgressIndicator(strokeWidth: 2,);
+    }
+
+    final actors = actorByMovie[movieId.toString()]!;
+    return SizedBox(
+      height: 300,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount:actors.length,
+          itemBuilder: (context, index) {
+            final actor = actors[index];
+
+            return Container(
+              padding: EdgeInsets.all(8.0),
+              width: 135,
+              child: Column(
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadiusGeometry.circular(20),
+                        child: Image.network(
+                          actor.profilePath,
+                          height: 180,
+                          width: 135,
+                          fit: BoxFit.cover,
+                          ),
+                      ),
+                      SizedBox(height: 5,),
+                      Text(actor.name, maxLines: 2,),
+                      Text(
+                        actor.character ?? '',
+                       maxLines: 2,
+                       style: TextStyle(overflow: TextOverflow.ellipsis),
+                       )
+                    ],
+              ),
+            );
+          },
+        
+      ),
+    );
+  }
+}
 
 class _CustomSliverAppbar extends StatelessWidget {
   final Movie movie;
@@ -127,7 +182,15 @@ class _CustomSliverAppbar extends StatelessWidget {
         background: Stack(
           children: [
             SizedBox.expand(
-              child: Image.network(movie.posterPath, fit: BoxFit.cover),
+              child: Image.network(
+                movie.posterPath, 
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if(loadingProgress != null){return SizedBox();}
+
+                  return  FadeIn(child: child);
+                },
+                ),
             ),
 
             SizedBox.expand(
@@ -173,10 +236,10 @@ class _CustomGradient extends StatelessWidget {
 
   const _CustomGradient({
     required this.primaryColor,
-    required this.secondaryColor, 
-    required this.stopStart, 
-    required this.stopEnd, 
-    required this.start, 
+    required this.secondaryColor,
+    required this.stopStart,
+    required this.stopEnd,
+    required this.start,
     required this.end,
   });
 
--- lib/presentation/provider/actor/actor_movie_respository.dart
+++ lib/presentation/provider/actor/actor_movie_respository.dart
@@ -1,30 +1,26 @@
-
-//creamos el provider 
+//creamos el provider
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/domain/entities/actor.dart';
 import 'package:movie_app/presentation/provider/actor/actor_provider.dart';
 
-//Creamos un provider que consulte el listado de actores 
-//Provider no inmutable 
+//Creamos un provider que consulte el listado de actores
+//Provider no inmutable
 
-final listActor = NotifierProvider<ActorsMovie, Map<String,List<Actor>>>(() => ActorsMovie() ,);
+final listActor = NotifierProvider<ActorsMovie, Map<String, List<Actor>>>(
+  () => ActorsMovie(),
+);
 
-//Clase que puedde consulta listado de actores 
-class ActorsMovie extends Notifier<Map<String,List<Actor>>> {
-
+//Clase que puedde consulta listado de actores
+class ActorsMovie extends Notifier<Map<String, List<Actor>>> {
   @override
-  Map<String,List<Actor>> build() {
+  Map<String, List<Actor>> build() {
     return {};
   }
 
-  //Metodo de consulta 
-  Future<void> getActorsMovie(String movieId )async{
-
-    if(state[movieId] != null) return;
-
-      final actorsMovie = await ref.read(actorProvider).getActorByMovie(movieId);
-
-    state ={...state, movieId:actorsMovie};
+  //Metodo de consulta
+  Future<void> getActorsMovie(String movieId) async {
+    if (state[movieId] != null) return;
+    final actorsMovie = await ref.read(actorProvider).getActorByMovie(movieId);
+    state = {...state, movieId: actorsMovie};
   }
-  
 }
