c884918275db4b6232680518ef7b4654f92a3d5d
Keviing <gualpakevin@gmail.com>
2026-02-20 23:05:07 -0500

    Implementacion de la busqueda usando search delegate

--- lib/presentation/delegates/delegate_movie.dart
+++ lib/presentation/delegates/delegate_movie.dart
@@ -1,12 +1,24 @@
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_app/domain/entities/movie.dart';
+  //creamos una funcion que realize la busqueda 
+                                  //Retorno de mi funcion y mi funcion con parametro
+  typedef SearchMovieCallback = Future<List<Movie>> Function(String query);
 
 class DelegateMovie extends SearchDelegate<Movie?> {
-  //Cambiar el texto dentro del text filed 
 
+  //agregamos la funcion al contructor de la clase delegate 
+  final SearchMovieCallback movieSearch;
+
+  DelegateMovie({required this.movieSearch});
+
+  
+
+  //Cambiar el texto dentro del text filed 
   @override
   String get searchFieldLabel => 'Buscar película';
+
+  //Acciones en el lado derecho de la barra de busqueda
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
@@ -21,6 +33,7 @@ class DelegateMovie extends SearchDelegate<Movie?> {
     ];
   }
 
+  //Acciones en el lado izquierdo de la barra de busqeda 
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
@@ -38,7 +51,24 @@ class DelegateMovie extends SearchDelegate<Movie?> {
 
   @override
   Widget buildSuggestions(BuildContext context) {
-    return Text('buildSuggestions');
+    
+    return FutureBuilder(
+      future:  movieSearch(query),
+      builder: (context, snapshot) {
+        
+        final movies = snapshot.data ?? [];
+
+        return ListView.builder(
+          itemCount: movies.length,
+          itemBuilder: (context, index) {
+            final movie = movies[index];
+            return ListTile(
+              title: Text(movie.title),
+            );
+          },
+          );
+      },
+      );
   }
   
 }
--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,11 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movie_app/presentation/delegates/delegate_movie.dart';
+import 'package:movie_app/presentation/provider/movie/movie_repository_provider.dart';
 
-class CustomAppbar extends StatelessWidget {
+class CustomAppbar extends ConsumerWidget {
   const CustomAppbar({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     final colors = Theme.of(context).colorScheme.primary;
     final titleStyle = Theme.of(context).textTheme.titleMedium;
     return  Padding(
@@ -22,9 +24,10 @@ class CustomAppbar extends StatelessWidget {
               Spacer(),
               IconButton(
                 onPressed: (){
+                  final movieRepository = ref.read(movieRepositoryProvider);
                   showSearch(
                     context: context, 
-                    delegate: DelegateMovie()
+                    delegate: DelegateMovie(movieSearch: movieRepository.searchMovie )
                     );
                 }, 
                 icon: Icon(Icons.search) 
